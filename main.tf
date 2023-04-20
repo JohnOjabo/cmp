11efe38df449ea39cea1a1f498be52a01f36048f
JohnOjabo <110981282+JohnOjabo@users.noreply.github.com>
2023-04-20 13:37:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role" "aws_access" {
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"]
 }
 
-resource "aws_iam_instance_profile" "instance_profile" {
+resource "aws_iam_instance_profile" "instance_profile1" {
   name = "${var.prefix}-profile"
   role = aws_iam_role.aws_access.name
 }
